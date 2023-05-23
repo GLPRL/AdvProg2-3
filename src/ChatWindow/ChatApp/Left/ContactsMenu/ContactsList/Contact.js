d51129bfe55046980304d4d7de6f00ce7e44527a
NaorGavriel <117026659+NaorGavriel@users.noreply.github.com>
2023-05-23 23:18:34 +0300

    working version

--- src/ChatWindow/ChatApp/Left/ContactsMenu/ContactsList/Contact.js
+++ src/ChatWindow/ChatApp/Left/ContactsMenu/ContactsList/Contact.js
@@ -15,21 +15,19 @@ function Contact(props) {
                                 }
                             })
         const contactMessages = await response.json();
-        let numOfMessages = 0;
-        for (const msg in contactMessages) {
-            numOfMessages++;
-        }
-        console.log("numOfContacts is " + numOfMessages)
-        date = contactMessages[numOfMessages-1].created;
+
         props.setCurrentContactMsgs(contactMessages);
         props.setCurrentContactImage(props.image);
         props.setCurrentContactDisplayName(props.name);
     }
+    
+    
+    let lastMsgDate = "dsa"
     const contact =
     <>
         <td><img src={`data:image/jpeg;base64, ${props.image}`} alt="" className="chat-profile-image rounded-circle"></img></td>
         <td className="chat-profile-name">{props.name}</td>
-        <td className="chat-date"><small>{date}</small></td>
+        <td className="chat-date"><small>{lastMsgDate}</small></td>
     </>
     return (
         <tr className="table-info" onClick={()=>handleContactClick(props.id)}>
