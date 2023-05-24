2f73d3ffb4892d53fca67909d5b541453eaf7c42
dekel98s <dekel98@gmail.com>
2023-05-24 20:10:59 +0300

    now checks that there is not conflict in username

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,9 +1,15 @@
-
+const User = require('../models/users');
 const userService = require('../services/users');
 const createUser = async (req,res) => {
     console.log("hii");
-
-    res.status(200).json(await userService.createUser(req.body.username,req.body.password,req.body.displayName,req.body.profilePic));
+    const username=req.body.username;
+    const user =await User.findOne({username});
+    if(user){
+        res.status(409).json({ title: "Conflict",status: "409"});
+    }
+    else {
+        res.status(200).json(await userService.createUser(req.body.username, req.body.password, req.body.displayName, req.body.profilePic));
+    }
 
 }
 
