e3160909292e46fc73767df609a635ac7f1def4f
NaorGavriel <117026659+NaorGavriel@users.noreply.github.com>
2023-05-27 12:17:48 +0300

    added correct respone for msg post

--- server/services/messages.js
+++ server/services/messages.js
@@ -1,26 +1,25 @@
 const mongoose = require('mongoose');
 const Message = require('../models/messages');
+const User = require('../models/users');
 
 const createMessage= async (chatId, currentUser, messageContent, messageId) =>{
     console.log("in createmsg in msgservice")
     const messagesModel = mongoose.model('messages', Message.schema, 'messages');
-
-    //const messagesCollection = await messagesModel.collection.find().toArray();
-    //console.log(messagesCollection)
-    console.log("---------")
+ 
     const userMessages = await messagesModel.findById(chatId)
-    console.log(userMessages);
-    const messageDetails = {_id: messageId, sender: {username:currentUser}, created : Date() ,content: messageContent}
-    console.log("---------")
-    console.log(messageDetails)
-    console.log("---------")
-    if (userMessages.messages == null) {
-        userMessages.messages = []
-    }
+    const currDate = Date()
+    const messageDetails = {_id: messageId, sender: {username:currentUser}, created : currDate ,content: messageContent}
     userMessages.messages.push(messageDetails);
-    console.log(messageDetails)
-    console.log(userMessages)
-    return await userMessages.save();
+
+    const user =await User.findOne({username: currentUser});
+    console.log(user);
+    const resData = {
+        id : messageId, created : currDate, sender : {
+            username: currentUser, displayName: user.displayName, profilePic : user.profilePic }, content: messageContent
+        }
+    console.log(resData);
+    await userMessages.save();
+    return resData
 }
 
 
