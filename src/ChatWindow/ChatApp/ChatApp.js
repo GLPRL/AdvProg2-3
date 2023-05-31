b2f0ad0694535c4855f10fa8c567dd33bce5b4ef
galpearl <galpearl24@gmail.com>
2023-05-31 22:36:05 +0300

    will no longer send message to everyone.

--- src/ChatWindow/ChatApp/ChatApp.js
+++ src/ChatWindow/ChatApp/ChatApp.js
@@ -3,7 +3,7 @@ import Right from "./Right/Right";
 import { useState } from "react";
 import { useEffect } from "react";
 function ChatApp(props) {
-    const [currentUser, setCurrentUser] = useState(null);
+    const [currentUser, setCurrentUser] = useState(0);
     const [firstPrint, setfirstPrint] = useState(true);
     const [currentChatId, setCurrentChatId] = useState(null);
     const [currentContactMsgs, setCurrentContactMsgs] = useState([])
@@ -12,15 +12,17 @@ function ChatApp(props) {
     useEffect(()=>{
     },[currentUser]);
 
+    console.log("ChatApp CURRENTUSER: " + currentUser);
+
     return (
         <div className="container content row no-gutters text">
             <Left contactIdAndTime={contactIdAndTime} setCurrentContactDisplayName={props.setCurrentContactDisplayName} 
-                  setCurrentContactImage={props.setCurrentContactImage} setCurrentUser={setCurrentUser} 
+                  setCurrentContactImage={props.setCurrentContactImage} setCurrentUser={setCurrentUser}
                   user={props.user} userContacts={props.userContacts} token={props.token} setCurrentChatId={setCurrentChatId} 
                   setCurrentContactMsgs={setCurrentContactMsgs} setUserContacts={props.setUserContacts}/>
             <Right setContactIdAndTime={setContactIdAndTime} currentContactDisplayName={props.currentContactDisplayName} 
                    currentContactMsgs={currentContactMsgs} currentContactImage={props.currentContactImage} 
-                   currentUser={currentUser} user={props.user} firstPrint={firstPrint} userContacts={props.userContacts} 
+                   currentUser={currentUser} user={props.user} firstPrint={firstPrint} userContacts={props.userContacts}
                    token={props.token} setfirstPrint={setfirstPrint} setCurrentContactMsgs={setCurrentContactMsgs} 
                    setUserContacts={props.setUserContacts}
                    />
--- src/ChatWindow/ChatApp/Right/ChatInteraction/ChatInteraction.js
+++ src/ChatWindow/ChatApp/Right/ChatInteraction/ChatInteraction.js
@@ -1,6 +1,7 @@
 import React from "react";
 import io from "socket.io-client"
-import { useEffect} from "react";
+import {useEffect} from "react";
+
 export const socket = io.connect("http://localhost:5000")
 
 function ChatInteraction(props) {
@@ -52,38 +53,39 @@ function ChatInteraction(props) {
         document.getElementById("outText").value = "";
         socket.emit("newMessage", props.currentUser);
     }
+
     const chat = props.currentUser;
     console.log("CHAT: " + chat)
-    useEffect(() => {
-        socket.on("receiveMessage", async (chatID) => {
-            console.log("CHATID ISSSSSSS: " + chatID)
-            console.log("CHAT ISSSSS: " + chat);
-            console.log("MESSAGE RECEIVED");
-            if (chat == chatID) {
-                console.log("----PAST IF----")
-                let userAdress = 'http://localhost:5000/api/Chats/' + props.currentUser + '/Messages'
-                let autor = 'Bearer ' + props.token
-                const responseGet = await fetch(userAdress, {
-                    method: 'GET',
-                    headers: {
-                        'Authorization': autor,
-                        'accept': 'text/plain',
-                    }
-                })
-                const contactMessages = await responseGet.json();
-                props.setCurrentContactMsgs(contactMessages);
-                const responseGetContacts = await fetch('http://localhost:5000/api/Chats/', {
-                    method: 'GET',
-                    headers: {
-                        'Authorization': autor,
-                        'accept': 'text/plain',
-                    }
-                })
-                const contacts = await responseGetContacts.json();
-                props.setUserContacts(contacts);
-            }
-        })
-    }, [socket]);
+
+    socket.on("receiveMessage", async (chatID) => {
+        console.log("CHATID ISSSSSSS: " + chatID)
+        console.log("CHAT ISSSSS: " + chat);
+        console.log("MESSAGE RECEIVED");
+        if (props.currentUser == chatID) {
+            console.log("----PAST IF----")
+            let userAdress = 'http://localhost:5000/api/Chats/' + props.currentUser + '/Messages'
+            let autor = 'Bearer ' + props.token
+            const responseGet = await fetch(userAdress, {
+                method: 'GET',
+                headers: {
+                    'Authorization': autor,
+                    'accept': 'text/plain',
+                }
+            })
+            const contactMessages = await responseGet.json();
+            props.setCurrentContactMsgs(contactMessages);
+            const responseGetContacts = await fetch('http://localhost:5000/api/Chats/', {
+                method: 'GET',
+                headers: {
+                    'Authorization': autor,
+                    'accept': 'text/plain',
+                }
+            })
+            const contacts = await responseGetContacts.json();
+            props.setUserContacts(contacts);
+        }
+    })
+
     return (
         <div className="sendLine">
             <input type="text" id="outText" className="textOut"></input>
